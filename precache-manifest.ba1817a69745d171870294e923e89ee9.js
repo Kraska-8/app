8cb13887cc5a7ccb8a17efa8636ccfea5138b48b
kraska-8 <kraska-8@yandex.ru>
2019-01-29 03:22:49 +0300

    Updates

---
+++ precache-manifest.ba1817a69745d171870294e923e89ee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "10256b2700850e285109",
+    "url": "/app/static/js/runtime~main.10256b27.js"
+  },
+  {
+    "revision": "92f71b3b2a303205d718",
+    "url": "/app/static/js/main.92f71b3b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/app/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "92f71b3b2a303205d718",
+    "url": "/app/static/css/main.ddcd64d8.chunk.css"
+  },
+  {
+    "revision": "9fbe8d9a44b4c8114f0e42bf832e35ba",
+    "url": "/app/index.html"
+  }
+];
